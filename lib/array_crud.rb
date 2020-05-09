976e9928bae62d9a208312804852155caa9db068
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-09 23:44:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   array=["dinosaurs","elephants","sharks","whales"]
-  animals.push("cows")
+   array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
